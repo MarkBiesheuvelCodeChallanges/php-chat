0348ebeae64a4ccb6439ba6e30f1ba5d278b3068
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-02-06 21:00:11 +0100

    Keep track of users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends BaseController
 {
+    const ONLINE_THRESHOLD = 60;
+
     public function index(Request $request)
     {
-        return response()->json([
-            [
-                'id'    => $request->userId,
-                'isYou' => true,
-            ],
-        ]);
+        // A user is online when they made a request a minute ago (now - 60s)
+        $onlineCutOff = time() - self::ONLINE_THRESHOLD;
+
+        $users = User::all()
+            ->map(function (User $user) use ($request, $onlineCutOff) {
+                return [
+                    'id'       => $user->id,
+                    'isOnline' => ($user->last_request > $onlineCutOff),
+                    'isYou'    => ($user->id === $request->user->id),
+                ];
+            });
+
+        return response()->json($users);
     }
 }
--- app/Http/Middleware/UserSession.php
+++ app/Http/Middleware/UserSession.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\User;
 use Closure;
 
 class UserSession
@@ -18,14 +19,31 @@ class UserSession
         $session = $request->session();
 
         if ($session->has('userId')) {
-            $userId = $session->get('userId');
+            $id = $session->get('userId');
+            $user = User::find($id);
         } else {
-            $userId = random_int(10000, 99999);
-            $session->put('userId', $userId);
+            $user = null;
         }
 
-        $request->userId = $userId;
+        if ($user === null) {
+            $id = random_int(10000, 99999);
 
-        return $next($request);
+            $user = new User();
+            $user->id = $id;
+
+            $session->put('userId', $id);
+        }
+
+        // Make sure to indicate the current user is online
+        $user->last_request = time();
+
+        //Add user to request and continue on
+        $request->user = $user;
+        $response = $next($request);
+
+        // Only update user after everything is done
+        $user->save();
+
+        return $response;
     }
 }
