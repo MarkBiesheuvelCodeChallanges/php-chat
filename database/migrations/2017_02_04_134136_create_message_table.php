4bbcbe2cee9c0565f286dc1fbc0fa175bdf030d9
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-02-06 20:38:54 +0100

    Add models and database tables

--- database/migrations/2017_02_04_134136_create_message_table.php
+++ database/migrations/2017_02_04_134136_create_message_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMessageTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->integer('from_user_id');
+            $table->integer('to_user_id');
+            $table->text('content');
+            $table->boolean('is_received');
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 }
